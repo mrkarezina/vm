2796abb04fc65cc89cbff39eb98c725c9d9f4d94
mrkarezina <mrk.arezina@gmail.com>
2020-12-03 01:20:06 -0500

    Text model cursor methods

--- src/text_model.cc
+++ src/text_model.cc
@@ -2,25 +2,29 @@
 
 using namespace std;
 
-TextModel::TextModel(string &filename) { 
+TextModel::TextModel(string &filename) {
+  lines = make_shared<vector<string>>();
 
-    lines = make_shared<vector<string>>();
-
-    // TODO: tmp before reading from file
-    lines->push_back("Test 1");
-    lines->push_back("Test 2");
+  // TODO: tmp before reading from file
+  lines->push_back("Test 1");
+  lines->push_back("Test 2");
 }
 
 TextModel::~TextModel() {}
 
 void TextModel::run() { render(); }
 
-const shared_ptr<vector<string>> TextModel::getLines() {
-    return lines;
-}
+const shared_ptr<vector<string>> TextModel::getLines() { return lines; }
+
+void TextModel::setX(int x) { cur_posn.x = x; }
+
+void TextModel::setY(int y) { cur_posn.y = y; }
+
+int TextModel::getX() { return cur_posn.x; }
+
+int TextModel::getY() { return cur_posn.y; }
 
 void TextModel::addView(unique_ptr<ViewBase> view) {
-  view->draw(this);
   views.push_back(move(view));
 }
 
--- src/text_model.h
+++ src/text_model.h
@@ -10,10 +10,15 @@
 
 class ViewBase;
 
+struct Cursor {
+  int x;
+  int y;
+};
+
 class TextModel {
   std::shared_ptr<std::vector<std::string>> lines;
-
   std::vector<std::unique_ptr<ViewBase>> views;
+  Cursor cur_posn{0, 0};
 
  public:
   TextModel(std::string &filename);
@@ -24,6 +29,10 @@ class TextModel {
   void render();
 
   const std::shared_ptr<std::vector<std::string>> getLines();
+  void setX(int x);
+  void setY(int y);
+  int getX();
+  int getY();
 };
 
 #endif
