b653475140cd5819e860f9cecca661603499127b
mrkarezina <mrk.arezina@gmail.com>
2020-12-09 00:05:00 -0500

    Display command so far

--- src/view-cmd-bar.cc
+++ src/view-cmd-bar.cc
@@ -12,6 +12,8 @@ void ViewCmdBar::draw(TextModel *model) {
   win->erase_w();
   if (model->is_write_mode()) {
     win->write_line("-- INSERT --", 0, 0);
+  } else if (model->get_cmd_so_far().size() > 0) {
+    win->write_line(model->get_cmd_so_far(), 0, 0);
   } else {
     win->write_line("\"" + model->get_file_name() + "\"", 0, 0);
   }
